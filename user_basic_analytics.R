5a7847705965f99b8ff25243c63e44da563024f0
jason9075 <jason9075@gmail.com>
2017-12-19 22:27:13 +0800

    add day3 example

--- user_basic_analytics.R
+++ user_basic_analytics.R
@@ -1,4 +1,4 @@
-
+### Day 2 ###
 user = read.csv("input/user.csv", stringsAsFactors=FALSE)
 
 #install.packages("stringr")
@@ -14,3 +14,23 @@ library(ggplot2)
 
 ggplot(result, aes(x=Month, y=MembersCount)) +
     geom_bar(stat="identity", fill="skyblue")
+
+### Day 3 ###
+
+orders = read.csv("input/orders.csv", stringsAsFactors=FALSE, fileEncoding="big5")
+
+buyer_list <- orders %>%
+    distinct(BUYERID)
+buyer_list <- buyer_list$BUYERID
+
+result <- user %>%
+    mutate(Month = as.Date(user$CREATETIME, "%m/%d/%Y %H:%M:%S")) %>%
+    mutate(Month = substring(Month,1,7)) %>%
+    mutate(HasBought = ID %in% buyer_list) %>%
+    group_by(Month, HasBought) %>%
+    summarise(MembersCount = n())
+
+ggplot(result, aes(x=Month, y=MembersCount, fill=HasBought)) +
+    geom_bar(stat="identity")
+
+
