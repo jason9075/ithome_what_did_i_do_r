ec9e6040f1626046d812e165560439c9e8bb2b66
jason9075 <jason9075@gmail.com>
2017-12-29 08:38:27 +0800

    add day12 example

--- crawler.R
+++ crawler.R
@@ -3,5 +3,18 @@
 url <- "http://data.coa.gov.tw/Service/OpenData/FromM/FarmTransData.aspx?FOTT=CSV"
 download.file(url, destfile = "downloaded/result.csv" , method = "curl")
                      
+### Day 12 ###
 
+start <- as.Date("2017-01-01")
+end   <- as.Date("2017-01-31")
+
+theDate <- start
+while (theDate <= end){
+    year <- as.integer(format(theDate,"%Y")) - 1911
+    month_date <- format(theDate,".%m.%d") 
+    chineseDate <- paste0(year, month_date)
+    download.file(paste0(url, "&StartDate=", chineseDate, "&EndDate=", chineseDate), 
+                  destfile = paste0("downloaded/file", chineseDate, ".csv"), method = "curl")
+    theDate <- theDate + 1                    
+}
 
