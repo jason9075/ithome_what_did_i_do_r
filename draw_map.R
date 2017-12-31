6271d22487022e0669a4e4510ed37be625c9c8fa
jason9075 <jason9075@gmail.com>
2017-12-31 18:20:31 +0800

    add day15 example

--- draw_map.R
+++ draw_map.R
@@ -12,4 +12,39 @@ google_map <- get_googlemap(center=c(121.52311,25.04126), zoom=12, maptype='sate
 ggmap(google_map) +
     geom_point(data=LatLngData, aes(x=Lng, y=Lat), colour='red')
 
+### Day 15 ###
+
+#install.packages("httr")
+#install.packages("rjson")
+library(httr)
+library(rjson)
+
+getLatLng <- function(address){
+ 
+    urlData <- GET(paste0("https://maps.googleapis.com/maps/api/geocode/json?language=zh-TW&address=", URLencode(address)))
+    jsonResult <- rjson::fromJSON(rawToChar(urlData$content))
+    Sys.sleep(1)
+    if(jsonResult$status != "OK"){
+        print(paste0("Google geocode API Error, Reason:", jsonResult$error_message))
+        return("error")
+    }
+    print("LatLng Got")
+    lat <<- jsonResult$results[[1]]$geometry$location$lat
+    lng <<- jsonResult$results[[1]]$geometry$location$lng
+    
+    return(paste(lat, lng, sep=","))
+}
+
+address_data = read.csv("input/address.csv", stringsAsFactors=FALSE, header=FALSE)
+
+result <- address_data %>%
+    rowwise() %>%
+    mutate(LatLng = getLatLng(V1)) %>%
+    filter(LatLng!="error") %>%
+    separate(LatLng, c("Lat", "Lng"), sep=",") %>%
+    mutate(Lat=as.numeric(Lat), Lng=as.numeric(Lng))
+
+ggmap(google_map) +
+    geom_point(data=result, aes(x=Lng, y=Lat), colour='red')
+
 
