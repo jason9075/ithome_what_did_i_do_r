cfe53a6ff4a48080a1e29387c5e2fd49ff6f60ce
jason9075 <jason9075@gmail.com>
2018-01-08 20:43:42 +0800

    add day22 example

--- ui.R
+++ ui.R
@@ -1,8 +1,45 @@
 library(shiny)
 
+selection <- c("A", "B", "C")
+
 shinyUI(fluidPage(
 
-  headerPanel("用戶資料表"),
-  tableOutput('userTable')
-  
+    fluidRow(
+        column(4, 
+               div(style = "background-color: GreenYellow;",
+                   h4("黃綠區塊"),
+                   sliderInput('sampleSize', 'Sample Size',
+                               min=10, max=1000, value=500,
+                               step=10, round=0),
+                   br(),
+                   checkboxInput('check1', '勾選按鈕1'),
+                   checkboxInput('check2', '勾選按鈕2')
+                )
+        ),
+        column(4, 
+               div(style = "background-color: Coral;",
+                   h4("橘色區塊"),
+                   selectInput('select1', '選擇1', selection),
+                   selectInput('select2', '選擇2', selection),
+                   selectInput('select3', '選擇3', selection),
+                   br(),
+                   numericInput("num", 
+                                h3("數字選擇"), 
+                                value = 1)
+               )
+        ),
+        column(4, 
+               div(style = "background-color: LightBlue;",
+                   h4("淡藍區塊"),
+                   actionButton("button", "按鈕"),
+                   br(),
+                   dateInput("date", 
+                             h3("Date input")),
+                   br(),
+                   textInput("text", h3("Text input"), 
+                             value = "Enter text...") 
+               )
+        )
+    )
+    
 ))
