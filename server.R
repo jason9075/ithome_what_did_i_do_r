9e4f4f0630014a65b239888a75921e6a0e68545a
jason9075 <jason9075@gmail.com>
2018-01-12 21:35:36 +0800

    add day26 example

--- server.R
+++ server.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(tidyr)
 library(dplyr)
+library(ggplot2)
+library(ggmap)
 source("draw_map_function.R")
 
 shinyServer(function(input, output) {
@@ -16,9 +18,33 @@ shinyServer(function(input, output) {
             mutate(Lat=as.numeric(Lat), Lng=as.numeric(Lng))
     })  
     
+    observeEvent(input$do, {
+        if (is.null(v$addressWithLatLng)) return()
+        
+        set.seed(input$seed)
+        kmeans = kmeans(x = v$addressWithLatLng[, c('Lat','Lng')], centers = input$k_value)
+        v$addressWithKMeans <- v$addressWithLatLng %>%
+            ungroup() %>%
+            mutate(category=kmeans$cluster)
+    })
+    
     output$addressTable <- renderTable({
         if (is.null(v$addressWithLatLng)) return(v$address)
         v$addressWithLatLng
+    })
+    
+    output$allAddressPlot <- renderPlot({
+        if (is.null(v$addressWithLatLng)) return()
+        ggmap(get_googlemap(center=c(121.52311,25.04126), zoom=12, maptype='satellite'), extent='device') +
+            geom_point(data=v$addressWithLatLng, aes(x=Lng, y=Lat), colour='red')
+    })
+    
+    output$classifiedAddressPlot <- renderPlot({
+        if (is.null(v$addressWithKMeans)) return()
+        ggmap(get_googlemap(center=c(121.52311,25.04126), zoom=12, maptype='satellite'), extent='device') +
+            geom_point(data=v$addressWithKMeans,
+                       size=1.8,
+                       aes(x=Lng, y=Lat, colour=factor(category)))
     })
 })
 
--- ui.R
+++ ui.R
@@ -5,10 +5,17 @@ shinyUI(fluidPage(
     fluidRow(
         column(3, 
                h4("API抓取經緯度"),
-               actionButton("request", "取資料")
+               actionButton("request", "取資料"),
+               hr(),
+               numericInput("seed", h3("種子碼"), value = 20180112),
+               numericInput("k_value", h3("k值"), value = 10),
+               actionButton("do", "執行")
         ),
         column(9, 
-               tableOutput('addressTable')
+               tabsetPanel(
+                   tabPanel("地址表單", tableOutput('addressTable')), 
+                   tabPanel("地址分佈圖", plotOutput("allAddressPlot", height = "800px")),
+                   tabPanel("K-means結果分佈圖", plotOutput("classifiedAddressPlot", height = "800px")))
         )
     )
     
